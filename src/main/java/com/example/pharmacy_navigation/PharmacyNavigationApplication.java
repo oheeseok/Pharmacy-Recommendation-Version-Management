884e8f06b66ad35245be7405f469e2e828af3767
oheeseok <heeseok96@gmail.com>
2024-06-06 23:13:58 +0900

    first commit

---
+++ src/main/java/com/example/pharmacy_navigation/PharmacyNavigationApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_navigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyNavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyNavigationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pharmacy_navigation/PharmacyNavigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_navigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyNavigationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
