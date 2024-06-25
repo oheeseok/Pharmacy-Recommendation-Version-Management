ef15a3042c91a1eecf6259d9a7abd556ba39e6c5
oheeseok <heeseok96@gmail.com>
2024-06-25 15:50:30 +0900

    JPA Auditing - BaseTime 테스트

--- src/test/groovy/com/example/pharmacy_navigation/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/pharmacy_navigation/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,7 +4,9 @@ import com.example.pharmacy_navigation.AbstractIntegrationContainerBaseTest
 import com.example.pharmacy_navigation.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
-class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest {
+import java.time.LocalDateTime
+
+class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
     private PharmacyRepository pharmacyRepository
@@ -57,5 +59,25 @@ class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest
 
         then:
         result.size() == 1
+    }
+
+    def "BaseTimeEntity 등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은해 약국"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
     }
 }
